20571800eee39e1e8dc98dc9937767d420bf0650
anndre-18 <sherrlynanndrina31@gamil.com>
2025-01-24 13:59:25 +0530

    login and regiter api

--- app.js
+++ app.js
@@ -2,16 +2,37 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const { v4 : uuidv4 } = require('uuid') ;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const cors = require("cors")
+const authMiddleware = require("./auth");
 
 
 app.use(express.json());//express middleware for POST method
+app.use(cors()) //to handle the cors error
 
 //connect backend with database
-mongoose.connect("mongodb+srv://anndre-18:sherrlyn31@cluster0.5afem.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")//returns a promise
-.then(()=>{
-    console.log("Connected to MongoDB")
-})
+// mongoose.connect("mongodb+srv://anndre-18:sherrlyn31@cluster0.5afem.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")//returns a promise
+// .then(()=>{
+//     console.log("Connected to MongoDB")
+// })
 
+
+mongoose
+  .connect("mongodb://localhost:27017/expenses") //returns a promise
+  .then(() => {
+    console.log("Connected to MongoDB");
+  });
+
+const userSchema= new mongoose.Schema({
+    id:{type:String,required:true,unique:true},
+    uname:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true}
+
+})
+const User = mongoose.model("User",userSchema)
 
 //creating a schema for DB
 const expenseSchema = new mongoose.Schema({
@@ -19,18 +40,19 @@ const expenseSchema = new mongoose.Schema({
     title:{type:String,required:true},
     amount:{type:Number,required:true},
 });
-//creating a model
+//creating a model for Expense
 const Expense = mongoose.model("Expense",expenseSchema);
 
 //GET method - to get all data
-app.get("/api/expenses",async(req,res)=>{
+app.get("/api/expenses",authMiddleware,async(req,res)=>{
+    console.log(req.user)
     try{
-
+        //await ensures that find operation is executed completely
         const expenses = await Expense.find();
-if(!expenses){
-    res.status(404).send({message: "No expenses found"});
-}
-res.status(200).json(expenses);
+        if(!expenses){
+            res.status(404).send({message: "No expenses found"});
+        }
+        res.status(200).json(expenses);
     }
     catch(error){
         res.status(500).json({message:"Internal server error"});
@@ -38,7 +60,7 @@ res.status(200).json(expenses);
 });
 
 //to get one data
-app.get("/api/expenses/:id",async(req,res)=>{
+app.get("/api/expenses/:id", authMiddleware, async(req,res)=>{
     const {id} = req.params;
     try{
 
@@ -53,12 +75,6 @@ catch(error){
     res.status(500).json({message:"Internal server error"});}
   });
     
-    
-
-
-
-
-
 
 // const expenses = [
 //   {
@@ -94,6 +110,8 @@ catch(error){
 //   res.status(200).json(expense);
 // });
 
+
+
 // POST METHOD
 app.post("/api/expenses", async(req, res) => {
 //   console.log(req.body); //request must be parsed for which we use middleware -> parse the data which comes from the client and then it is sent to the server
@@ -114,7 +132,8 @@ try{
 
 }
 catch(error){
-    res.status(500).json({message:"Internal server error"});}
+    res.status(500).json({message:"Internal server error"});
+}
 
 });
 
@@ -158,6 +177,47 @@ app.put("/api/expenses/:id", async (req, res) => {
   });
 
 
+app.post("/register",async(req,res)=>{
+    const { email ,uname, password}=req.body;
+    try{
+        const user = await User.findOne({email});
+        if(user){
+            res.status(400).json({message:"email alresdy exist"})
+            return
+        }
+        const hashedPassword = await bcrypt.hash(password ,10)
+        const newUser = new User({
+            id: uuidv4(),
+            email,uname,
+            password: hashedPassword
+        })
+        await newUser.save()
+        res.status(200).json({message:"User created success"})
+    }
+    catch(error){
+        res.status(500).json({message:"Internal Server error"})
+    }
+})
+
+app.post("/login",async(req,res)=>{
+    const {email,password}=req.body
+    try {
+        const existingUser= await User.findOne({email});
+       if(!existingUser){
+        return res.status(400).json({message:"Invalid email"})
+       }
+       const validPassword = await bcrypt.compare(password , existingUser.password)
+       if(!validPassword){
+        return res.status(400).json({message:"Invalid Password"})
+       }
+       const token = jwt.sign({id: existingUser.id},"my_secret",{expiresIn : '1h'})
+       res.status(200).json({token})
+       
+    } catch (error) {
+        res.status(500).json({message:"Internal Server error"})
+        
+    }
+})
 app.listen(3000, () => {
   console.log("server is running");
 });
